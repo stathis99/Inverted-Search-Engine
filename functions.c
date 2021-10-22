7987115c7cf3ccb1bb9fbf1cc346f04ca8db9cf4
sdi1700168 <dimtsiamouras@gmail.com>
2021-10-22 18:11:04 +0300

    Create and Destroy Entry Functions

--- functions.c
+++ functions.c
@@ -2,43 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-//can this even work?
-enum error_code create_entry1(const word* w, entry* e){
-    printf("gets\n");
-    //printf("%s",w->key_word);
-    e = (entry*)malloc(sizeof(entry)); //this line causes segmentation
-    e->this_word = (word*)malloc(sizeof(word));
-    e->this_word->key_word = (char*)malloc(sizeof(char*)*strlen(w->key_word));
-    strcpy(e->this_word->key_word,w->key_word);
-    //e->this_word = (word*)w;
-    enum error_code my_enum = SUCCESS;
-    return my_enum;
-}
-
 enum error_code create_entry(const word* w, entry* e){
     //e->this_word = (word*)malloc(sizeof(word));
     /*e->this_word->key_word = (char*)malloc(sizeof(char*)*strlen(w->key_word));
     strcpy(e->this_word->key_word,w->key_word);*/
-    
-    e->this_word = (word*)w;
+    //*e = malloc(sizeof(entry));
+    *e = malloc(sizeof(Entry));
+    (*e)->this_word = (word*)w;
+    (*e)->payload = malloc(sizeof(char*)*2);
+    char payload1[] = "str2";
+    (*e)->payload[0] = malloc(sizeof(payload1)*strlen(payload1));
+    strcpy((*e)->payload[0],payload1);
     //printf("e->this_word->key_word:%s\n",e->this_word->key_word);
     enum error_code my_enum = SUCCESS;
     return my_enum;
 }
 
-/*Entry* create_entry(){
-    Entry* my_entry = (Entry*)malloc(sizeof(Entry));
-    char string1[] = "str1";
-    my_entry->word = (char*)malloc(strlen(string1)*sizeof(string1));
-    strcpy(my_entry->word,string1);
-    char string2[] = "str12";
-    char string3[] = "str13";
-    char** array = malloc (sizeof(char*));
-    array[]
-
-    /*char[] string2 = "str2";
-    char* string3 = malloc(sizeof);
-    my_entry->word = NULL;
-    my_entry->payload = NULL;
-    return my_entry;
-}*/
+enum error_code destroy_entry(entry* e){
+    free((*e)->payload[0]);
+    free((*e)->payload);
+    free((*e)->this_word->key_word);
+    free((*e)->this_word);
+    free((*e));
+}
--- structs.h
+++ structs.h
@@ -4,12 +4,15 @@ typedef struct word{
     char* key_word;
 }word;
 
-typedef struct entry{
-    word* this_word;
+typedef struct{
     char** payload;
-}entry;
+    word* this_word;
+}Entry;
 
+typedef Entry *entry;
 enum error_code { SUCCESS = 0, ERROR = 1};
 //Entry* create_entry();
 
+//enum error_code create_entry(const word* w, entry** e);
 enum error_code create_entry(const word* w, entry* e);
+enum error_code destroy_entry(entry* e);
