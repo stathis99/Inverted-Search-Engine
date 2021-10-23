3c3fc875f1d79584e3ceda8ebbb581d74f848879
sdi1700168 <dimtsiamouras@gmail.com>
2021-10-23 10:18:51 +0300

    Revert "Revert "Create, Number & Destroy entry list""
    This reverts commit f61511c107341fc5966461b5b2bf600fc779236a.

--- structs.h
+++ structs.h
@@ -8,11 +8,21 @@ typedef struct{
     char** payload;
     word* this_word;
 }Entry;
-
 typedef Entry *entry;
+
+typedef struct Entry_List{
+    entry entry_node;
+    struct Entry_List *next;
+}Entry_List;
+typedef Entry_List *entry_list;
+
 enum error_code { SUCCESS = 0, ERROR = 1};
 //Entry* create_entry();
 
 //enum error_code create_entry(const word* w, entry** e);
 enum error_code create_entry(const word* w, entry* e);
 enum error_code destroy_entry(entry* e);
+enum error_code create_entry_list(entry_list* el);
+enum error_code destroy_entry_list(entry_list* el);
+unsigned int get_number_entries(const entry_list* el);
+enum error_code add_entry(entry_list* el, const entry* e);
